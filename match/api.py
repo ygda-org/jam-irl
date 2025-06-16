685ba0212d867e0c2fa4c7be9b9c122a616fbd92
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2025-06-16 18:39:29 -0400

    better health check

--- match/api.py
+++ match/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from routes import user, match
+from lib.db import prisma
 
 router = APIRouter()
 
@@ -8,5 +9,27 @@ router.include_router(match.router, prefix="/match", tags=["matches"])
 
 @router.get("/")
 async def health_check():
-    return {"status": "healthy"}
+
+    try: 
+        matches = await prisma.match.find_many(
+            where={
+                "status": {"not": "FINISHED"}
+            },
+        )
+    except Exception as e:
+        return {
+            "status": "unhealthy",
+            "error": str(e)
+        }
+    
+    return {
+        "status": "healthy",
+        "matches": [
+            {
+                "code": m.code,
+                "gsiUrl": m.gsiUrl
+            }
+            for m in matches
+        ]
+    }
 
