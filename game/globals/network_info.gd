7adcac5ffb7ea315f4ac56d8e07745ddc02d2ff0
sroshc <shirzaysroshsafa@gmail.com>
2025-06-16 14:53:32 -0400

    Client requests for userID

--- game/globals/network_info.gd
+++ game/globals/network_info.gd
@@ -5,10 +5,19 @@ enum State {
 	Server
 }
 
+## Match making server info:
+@export var match_making_address = "http://localhost:8000"
+
+###
+
+## Game instance info:
+@export var user_id: String = "none"
+
 @export var state: State = State.Server
-@export var address: String = "localhost"
 @export var port: int = 9999 # 9999 is default game instance port, docker container binds it to port 10000-19999 on host machine
 @export var code: String = "default"
+@export var address: String = "localhost"
+##
 
 func get_address_with_port(tls: bool = false) -> String:
 	var protocol: String = ["ws://", "wss://"][int(tls)]
--- game/start_menu.gd
+++ game/start_menu.gd
@@ -7,15 +7,30 @@ extends Node2D
 func _ready() -> void:
 	var args: = OS.get_cmdline_args()
 	
-	if args.has("--port"):
-		NetworkInfo.port = int(args[args.find("--port") + 1])
-	
-	if args.has("--code"):
-		NetworkInfo.code = args[args.find("--code") + 1]
-	
-	if args.has("--server"):
+	if args.has("--server"): # If it's a server, begin
+		if args.has("--port"):
+			NetworkInfo.port = int(args[args.find("--port") + 1])
+		
+		if args.has("--code"):
+			NetworkInfo.code = args[args.find("--code") + 1]
+		
 		SceneSwitcher.goto_scene("res://game/game.tscn")
-	
+	else: # If it's a user, request an ID
+		%UserIDRequest.request_completed.connect(on_userid_request_completed)
+		var headers = ["Content-Type: applications/json"]
+		%UserIDRequest.request(NetworkInfo.match_making_address + "/user", headers, HTTPClient.METHOD_POST)
+
+func on_userid_request_completed(result, response_code, headers, body) -> void:
+	print(str(response_code))
+	print(str(result))
+	print(str(headers))
+	print(str(body))
+	if response_code == 200:
+		var json = JSON.parse_string(body.get_string_from_utf8())
+		NetworkInfo.user_id = json["userID"]
+		GlobalLog.client_log("Received user id from matchmaking server: " + NetworkInfo.user_id)
+	else:
+		GlobalLog.client_log("Failed to request for a userid from matchmaking server.")
 		
 
 func _on_button_pressed() -> void:
@@ -26,4 +41,6 @@ func _on_button_pressed() -> void:
 	SceneSwitcher.goto_scene("res://game/game.tscn")
 
 func _on_debug_server_pressed() -> void:
+	
 	SceneSwitcher.goto_scene("res://game/game.tscn")
+	
