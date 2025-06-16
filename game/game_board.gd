be9e3e7c3de9919df42437e1185bbc6237ca3f04
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-06-16 15:57:00 -0400

    GameBoard

---
+++ game/game_board.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var board = []
+
+const FLOOR = preload("res://game/alice_placeables/floor.tscn")
+const TOWER = preload("res://game/alice_placeables/tower.tscn")
+
+func _ready():
+	initializeBoard()
+	updateTile(TOWER, 7, 2)
+
+func initializeBoard():
+	for r in range(10):
+		board.append([])
+		for c in range(12):
+			board[r].append(FLOOR.instantiate())
+			board[r][c].position = Vector2(c*50, r*50)
+			add_child(board[r][c])
+
+
+func updateTile(newTileConst, r, c):
+	var newTile = newTileConst.instantiate()
+	var oldPos = board[r][c].position
+	newTile.position = oldPos
+	board[r][c].queue_free()
+	board[r][c] = newTile
+	add_child(newTile)
