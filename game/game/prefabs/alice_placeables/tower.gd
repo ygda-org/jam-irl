11c9b6bd07174f5818e609979bb3145a384a4715
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-06-17 15:32:55 -0400

    Halfway again lol

--- game/game/prefabs/alice_placeables/tower.gd
+++ game/game/prefabs/alice_placeables/tower.gd
@@ -3,11 +3,13 @@ extends StaticBody2D
 @onready var bob = get_parent().get_parent().get_node("Bob")
 @onready var game = get_parent().get_parent().get_parent()
 
+var projectile = load("res://game/game/GenericProjectile/ProjectileSettingResources/tower_shot.tres")
+
 var bob_in_range = false
 
 
-func attack():
-	game.create_projectile(FiringPosition.position)
+func attack() -> void:
+	game.summon_projectile($FiringPosition.position, $FiringPosition.position.direction_to(bob.position), projectile)
 
 func _on_range_body_entered(body: Node2D) -> void:
 	if body.name == "Bob":
@@ -21,4 +23,6 @@ func _on_range_body_exited(body: Node2D) -> void:
 
 
 func _on_cooldown_timeout() -> void:
-	pass # Replace with function body.
+	if bob_in_range:
+		attack()
+		$Cooldown.start()
