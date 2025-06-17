60dde1b7c438d5026df7e85ff9f5529f26366f38
George Taylor <george.taylor.academics@gmail.com>
2025-06-17 15:28:42 -0400

    Money Mechanic + Animations
    !

--- game/game/game/alice_controller.gd
+++ game/game/game/alice_controller.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 const FLOOR = preload("res://game/prefabs/alice_placeables/floor.tscn") # 0
 const TOWER = preload("res://game/prefabs/alice_placeables/tower.tscn") # 1
+const WALL = preload("res://game/prefabs/alice_placeables/wall.tscn") # 2
+const SWEATSHOP = preload("res://game/prefabs/alice_placeables/sweatshop.tscn") # 3
 const margin = 20
 
-const structures = [FLOOR, TOWER]
+const structures = [FLOOR, TOWER, WALL, SWEATSHOP]
+const costs = [0,4,1,5]
 
 var mouse_position: Vector2
 var current_selected
@@ -12,6 +15,8 @@ var current_selected
 @onready var board = get_parent().get_node("Arena").get_node("GameBoard")
 
 var health = 4
+var money = 20
+var number_of_children_worked_to_the_bone = 0
 
 func _process(delta):
 	mouse_position = get_viewport().get_mouse_position()
@@ -29,8 +34,10 @@ func placeTile():
 	var mouse_pos = mouse_position
 	mouse_pos = (mouse_pos - Vector2(margin, margin)) / 50
 	mouse_pos = Vector2i(mouse_pos)
-	board.rpc("updateTile", current_selected, mouse_pos.y, mouse_pos.x)
-	board.updateTile(current_selected, mouse_pos.y, mouse_pos.x)
+	if money - costs[current_selected] >= 0:
+		board.rpc("updateTile", current_selected, mouse_pos.y, mouse_pos.x)
+		board.updateTile(current_selected, mouse_pos.y, mouse_pos.x)
+		change_money(-costs[current_selected])
 
 func _on_tower_pressed() -> void:
 	$PlacePreview.play("tower")
@@ -38,9 +45,9 @@ func _on_tower_pressed() -> void:
 	current_selected = 1
 
 func _on_wall_pressed() -> void:
-	$PlacePreview.play("wall")
+	$PlacePreview.play("wallH")
 	#get_node("PlacePreview").texture = load("res://ASSETS/placeables/tower.png")
-	current_selected = 1
+	current_selected = 2
 
 func _on_floortest_pressed() -> void:
 	$PlacePreview.play("floorTEST")
@@ -50,8 +57,23 @@ func _on_floortest_pressed() -> void:
 
 func dec_health() -> void:
 	health -= 1
-	health %= 4
-	for idx in range(health,$HealthBar.get_child_count()):
-		$HealthBar.get_child(idx).visible = false
 	if health <= 0:
 		print("Allice died :(")
+		health = 0
+	for idx in range($HealthBar.get_child_count()-health):
+		$HealthBar.get_child(idx).play("turbo_dead")
+
+func change_money(change) -> void:
+	money += change
+	if money < 0:
+		money = 0
+	$HealthBar/Money.text = "$"+str(money)
+
+func _on_sweatshop_pressed() -> void:
+	number_of_children_worked_to_the_bone += 1
+	$PlacePreview.play("sweatshop")
+	
+	current_selected = 3
+
+func _on_money_timer_timeout() -> void:
+	change_money(number_of_children_worked_to_the_bone)
