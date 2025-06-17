d45b62939cbb8542220ce25818bbaa3aed87f924
MrPuffyWuff <jasper13.com@gmail.com>
2025-06-17 18:42:28 -0400

    Destructable Heart

--- game/game/prefabs/alice_heart.gd
+++ game/game/prefabs/alice_heart.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends StaticBody2D
 
 var health = 100
 const max_health = 100
@@ -6,8 +6,8 @@ const max_health = 100
 func _ready():
 	pass
 
-func hit(dmg: int):
-	health -= dmg
+
+func health_check():
 	if health <= 0:
 		$Anim.play("turbo_dead")
 	elif health <= max_health/4:
@@ -16,3 +16,23 @@ func hit(dmg: int):
 		$Anim.play("slight_hurt")
 	else:
 		$Anim.play("healthy")
+
+func _on_target_on_death() -> void:
+	GlobalLog.server_log(str(self) + " has died!")
+	suicide()
+
+func suicide():
+	rpc("_suicide")
+	_suicide()
+
+@rpc("authority")
+func _suicide():
+	queue_free()
+
+func _on_target_on_damage(damage: int) -> void:
+	health = $Target.health
+	health_check()
+	GlobalLog.server_log(str(self) + " has taken " + str(damage) + " damage!")
+
+func _to_string() -> String:
+	return "HEART: " + str(global_position)
