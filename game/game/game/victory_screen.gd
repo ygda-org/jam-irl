0c3dbb7fd3d42f25e31e592e250a9efef07b875d
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2025-06-17 17:47:45 -0400

    Bob can die

--- game/game/game/victory_screen.gd
+++ game/game/game/victory_screen.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+@onready var winner_label: Label = $Label
+
+func _ready() -> void:
+	var winner: String = ""
+	if GameManager.winner == NetworkManager.Role.Alice:
+		winner = "Alice"
+	else:
+		winner = "Bob"
+	winner_label.text = winner + " wins!"
 
 func _on_home_pressed() -> void:
 	SceneSwitcher.goto_start()
