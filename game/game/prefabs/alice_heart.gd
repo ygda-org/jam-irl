ab7d0470ab31e1599a666ecc15c769cb20aa8540
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-06-17 13:55:42 -0400

    Make heart actually work lol

--- game/game/prefabs/alice_heart.gd
+++ game/game/prefabs/alice_heart.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
 var health = 100
+const max_health = 100
+
+func _ready():
+	hit(99)
 
 func hit(dmg: int):
-	var old = health
-	health -= hit
+	health -= dmg
 	if health <= 0:
 		$Anim.play("turbo_dead")
-	elif health <= health/4:
+	elif health <= max_health/4:
 		$Anim.play("turbo_cancer")
-	elif health <= health/2:
+	elif health <= max_health/2:
 		$Anim.play("slight_hurt")
 	else:
 		$Anim.play("healthy")
