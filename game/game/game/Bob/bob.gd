79ab98a2ae1a7a818fcf0538d7409d2caea2438a
sroshc <shirzaysroshsafa@gmail.com>
2025-06-17 18:55:59 -0400

    added bob attacking

--- game/game/game/Bob/bob.gd
+++ game/game/game/Bob/bob.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 const SPEED: int = 10000
+const ATTACK_COOLDOWN: float = 0.75
+
+var can_attack: bool = true
+
 var input: Vector2 = Vector2(0, 0)
 @onready var game = get_tree().root.get_node("Game")
 @onready var bob_ui = get_node("../../BobUI")
@@ -42,9 +46,25 @@ func _on_target_on_damage(damage: int) -> void:
 	GlobalLog.server_log("Bob has taken " + str(damage) + " damage!")
 	rpc("__on_target_on_damage", damage)
 
+@rpc("any_peer")
+func attack():
+	if can_attack:
+		can_attack = false
+		%BobAttackCooldown.start(ATTACK_COOLDOWN)
+		%Anim.play("sword1")
+		var attacked_bodies: Array[Node2D] = %BobAttackArea.get_overlapping_bodies()
+		for body in attacked_bodies:
+			if body.is_in_group("Damageable"):
+				body.damage()
+		
+
 @rpc("authority")
 func __on_target_on_damage(damage: int) -> void:
 	bob_ui.update_health_bar(health())
 
 func _to_string() -> String:
 	return "BOB"
+
+
+func _on_bob_attack_cooldown_timeout() -> void:
+	can_attack = true
