52248f82104de8f24afc0853e38fc6e2022e9bd5
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2025-06-17 16:59:32 -0400

    slime works!

--- game/game/game/slime/slime.gd
+++ game/game/game/slime/slime.gd
@@ -9,7 +9,7 @@ var jumpTime: float = 6./12.;
 @export var target: Node2D
 
 @onready var board = get_parent().get_node("GameBoard")
-
+@onready var attack_area: Area2D = $AttackArea
 
 @export var is_spawning: bool = true
 @export var is_jumping: bool = false
@@ -22,7 +22,6 @@ func _ready() -> void:
 		push_error("Bob not found in scene!")
 	
 	$Anim.play("Spawn")
-
 
 func _physics_process(delta: float) -> void:
 	if can_jump:
@@ -45,8 +44,18 @@ func start_jump() -> void:
 	is_jumping = false
 	jump_target = null
 
+	if is_target_in_attack_range():
+		target.get_node("Target").damage(damage)
+
 	await get_tree().create_timer(jumpCooldown).timeout
 	can_jump = true
+
+func is_target_in_attack_range() -> bool:
+	if target == null:
+		return false
+		
+	var bodies = attack_area.get_overlapping_bodies()
+	return bodies.has(target)
 
 
 func _on_anim_animation_finished() -> void:
