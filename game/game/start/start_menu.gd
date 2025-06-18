767cb92c071c8d6ba5a7ee4b465a1e039cc13234
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2025-06-18 17:35:53 -0400

    small ui bump

--- game/game/start/start_menu.gd
+++ game/game/start/start_menu.gd
@@ -10,21 +10,9 @@ func _ready() -> void:
 	var args: = OS.get_cmdline_args()
 	_parse_args(args)
 	
-	if args.has("--server"): # If it's a server, begin
-		NetworkManager.state = NetworkManager.State.Server
-		if args.has("--port"):
-			NetworkManager.port = int(args[args.find("--port") + 1])
-		
-		if args.has("--code"):
-			NetworkManager.code = args[args.find("--code") + 1]
-		
-		if args.has("--match-id"):
-			NetworkManager.match_id = args[args.find("--match-id") + 1]
-		
+	if NetworkManager.is_server():
 		SceneSwitcher.goto_lobby()
 	else: # If it's a user, request an ID
-		NetworkManager.state = NetworkManager.State.Client
-		
 		if NetworkManager.user_id != "none": # If the client already has a userid, don't request a new one
 			return
 		
@@ -49,6 +37,9 @@ func _parse_args(args: PackedStringArray) -> void:
 		NetworkManager.state = NetworkManager.State.Client
 		if args.has("--user-id"):
 			NetworkManager.user_id = args[args.find("--user-id") + 1]
+	
+	# if args.has("--mute"):
+	# 	Settings.mute = true
 
 func _on_debug_client_pressed() -> void:
 	_to_lobby(NetworkManager.State.Client, address.text + ":" + port.text, code.text)
