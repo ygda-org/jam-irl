40400b2a51d378c2e795f032ce0b99ad4338e372
MrPuffyWuff <jasper13.com@gmail.com>
2025-06-17 20:25:02 -0400

    More Music

--- game/globals/music_manager/music_manager.gd
+++ game/globals/music_manager/music_manager.gd
@@ -6,19 +6,30 @@ var sound_effect_dict : Dictionary[SoundEffectSettings.SOUND_EFFECT_LABEL, Sound
 
 var general_music_playing = false
 
+var prev_scene_name = ""
+
 func _ready() -> void:
 	for setting : SoundEffectSettings in sound_effect_settings:
 		sound_effect_dict[setting.label] = setting
 
 func _process(delta: float) -> void:
 	var current_scene = get_parent().get_child(6)
-	if current_scene.name == "StartScreen" and not general_music_playing:
+	if current_scene.name != prev_scene_name:
+		clear_all_audio()
+		general_music_playing = false
+	if general_music_playing: return
+	if current_scene.name == "StartScreen" or current_scene.name == "Lobby":
+		general_music_playing = true
+		create_audio(SoundEffectSettings.SOUND_EFFECT_LABEL.LOBBYJAM)
+	if current_scene.name == "Game" and not general_music_playing:
 		general_music_playing = true
 		create_audio(SoundEffectSettings.SOUND_EFFECT_LABEL.BATTLE_A)
+	
+	prev_scene_name = current_scene.name
 
 func _on_audio_finished(source : AudioStreamPlayer):
 	if int(source.name) < 4:
-		GlobalLog.log("YES THANK GOD")
+		#GlobalLog.log("YES THANK GOD")
 		general_music_playing = false
 
 @rpc("any_peer")
